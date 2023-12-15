e923ad1096376e985a080c1215644ec5c5e74e4e
clclt <clclclclclt@naver.com>
2023-12-15 10:29:37 +0900

    Feat: 에러 페이지 추가

---
+++ src/components/fix/Error.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import './Error.css';
+import './index.css';
+
+const Error = () => {
+
+
+    return (
+        <div className="error">
+            <div className='center-container'>
+            <Link to="/">
+            <div className='errorDesign'>
+                <img className='img-fluid' src={process.env.PUBLIC_URL + './error.jpg'} alt='에러페이지' />
+                <span className='errorMessage'>페이지를 찾을 수 없습니다.<br />누르면 홈으로 이동합니다.</span>
+            </div>
+            </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Error;
