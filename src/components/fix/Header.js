eb1f9c88db735c852c12a60793e6ec91fd6b0940
clclt <clclclclclt@naver.com>
2023-12-15 09:58:00 +0900

    Feat: 메뉴에 링크추가, 좌우양옆 간격 조절 추가

--- src/components/fix/Header.js
+++ src/components/fix/Header.js
@@ -31,11 +31,11 @@ const Header = () => {
                                 
                                 {/* menu start */}
                                 <ul className="nav">
-                                    <li><Link to="#gameDetail">Game</Link></li>
+                                    <li><Link to="/for-team">Game</Link></li>
                                     <li><Link to="#features">Rank</Link></li>
-                                    <li><Link to="#work-process">Teams</Link></li>
-                                    <li><Link to="#testimonials">MyPage</Link></li>
-                                    <li><Link to="#testimonials">Login</Link></li>
+                                    <li><Link to="/teams">Teams</Link></li>
+                                    <li><Link to="/user/mypage">MyPage</Link></li>
+                                    <li><Link to="/user/login">Login</Link></li>
 
                                 </ul>
                                 <a className='menu-trigger' onClick={menuClick}>
