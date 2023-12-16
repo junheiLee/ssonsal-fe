c80177506a2a3f6540083cc052c057ba8f1a6bd1
syl0415 <137062383+syl0415@users.noreply.github.com>
2023-12-16 21:38:35 +0900

    Feat: 리뷰 경로 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import { Suspense, lazy } from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Routes, Route, Outlet } from 'react-router-dom';
-import { lazy, Suspense } from 'react'
-import Header from './components/fix/Header';
 import Error from './components/fix/Error';
 import Footer from './components/fix/Footer';
+import Header from './components/fix/Header';
 import Loading from './components/fix/Loading';
 
 import Main from './pages/Main';
@@ -14,6 +14,10 @@ const TeamDetail = lazy(() => import('./pages/team/TeamDetail'));
 const TeamManager = lazy(() => import('./pages/team/TeamManager'));
 const TeamForm = lazy(() => import('./pages/team/TeamForm'));
 const TeamEdit = lazy(() => import('./pages/team/TeamEdit'));
+const ReportReviews = lazy(()=> import('./pages/review/ReportReviews'));
+const ReviewForm = lazy(()=> import ('./pages/review/ReviewForm'));
+const SubReviews = lazy(()=> import ('./pages/review/SubReviews'));
+const TeamReviews = lazy(()=> import ('./pages/review/TeamReviews'));
 
 
 const App = () => {
@@ -33,6 +37,10 @@ const App = () => {
           <Route path='*' element={<Error />} />
           <Route path='/teams/form' element={<TeamForm />} />
           <Route path='/teams/:id/edit' element={<TeamEdit />} />
+          <Route path="/reviews/team/:teamId" element={<TeamReviews />} />
+          <Route path="/reviews/sub/:userId" element={<SubReviews />} />
+          <Route path="/review" element={<ReviewForm />} />
+          <Route path="/report" element={<ReportReviews />} />
 
         </Routes>
       </Suspense>
