1d903021d960a3decbab00ac504beb332eded4d8
clclt <clclclclclt@naver.com>
2023-12-17 23:07:32 +0900

    Feat: 리뷰 페이지 수정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Error from './components/fix/Error';
 import Footer from './components/fix/Footer';
 import Header from './components/fix/Header';
 import Loading from './components/fix/Loading';
-
 import Main from './pages/Main';
 
 const Rank = lazy(() => import('./pages/rank/Rank'));
@@ -14,17 +13,17 @@ const TeamDetail = lazy(() => import('./pages/team/TeamDetail'));
 const TeamManager = lazy(() => import('./pages/team/TeamManager'));
 const TeamForm = lazy(() => import('./pages/team/TeamForm'));
 const TeamEdit = lazy(() => import('./pages/team/TeamEdit'));
-const ReportReviews = lazy(()=> import('./pages/review/ReportReviews'));
-const ReviewForm = lazy(()=> import ('./pages/review/ReviewForm'));
-const SubReviews = lazy(()=> import ('./pages/review/SubReviews'));
-const TeamReviews = lazy(()=> import ('./pages/review/TeamReviews'));
+const SubReviews = lazy(() => import('./pages/review/SubReviews'));
+const TeamReviews = lazy(() => import('./pages/review/TeamReviews'));
+const ReportForm = lazy(() => import('./pages/review/ReportForm'));
 
 
 const App = () => {
+
   return (
     <div className="App">
 
-<Suspense fallback={<Loading />}>
+      <Suspense fallback={<Loading />}>
         <Routes>
           <Route element={<MainLayout />}>
             <Route path='/' element={<Main />} />
@@ -32,16 +31,15 @@ const App = () => {
             <Route path='/teams/:id' element={<TeamDetail />} />
             <Route path='/teams/:id/managers' element={<TeamManager />} />
             <Route path='/ranks' element={<Rank />} />
+            <Route path="/reviews/team/:teamId" element={<TeamReviews />} />
+            <Route path="/reviews/:reviewId/report" element={<ReportForm />} />
+            <Route path="/reviews/sub/:userId" element={<SubReviews />} />
+
           </Route>
 
           <Route path='*' element={<Error />} />
           <Route path='/teams/form' element={<TeamForm />} />
           <Route path='/teams/:id/edit' element={<TeamEdit />} />
-          <Route path="/reviews/team/:teamId" element={<TeamReviews />} />
-          <Route path="/reviews/sub/:userId" element={<SubReviews />} />
-          <Route path="/review" element={<ReviewForm />} />
-          <Route path="/report" element={<ReportReviews />} />
-
         </Routes>
       </Suspense>
 
@@ -53,12 +51,12 @@ const App = () => {
 const MainLayout = () => {
   return (
     <>
-    <div className='wrapper'>
-      <Header />
-      <div className="main-content contentWrapper">
-      <Outlet />
-      </div>
-      <Footer />
+      <div className='wrapper'>
+        <Header />
+        <div className="main-content contentWrapper">
+          <Outlet />
+        </div>
+        <Footer />
       </div>
     </>
   );
